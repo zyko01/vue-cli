02ae125ba6c5771aa8b0b9cf94810ee0a19f92ef
chris <chris.praxxys@gmail.com>
2020-08-14 20:11:41 +0800

    Install vue cli

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="">
+		
+		<h2>The Count Is: {{ count }}</h2>
+
+		<p>
+			<button @click="count += 1">Increment</button>
+
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+
+		data() {
+			return {
+				count: 0
+			};
+		} 
+	}
+</script>
